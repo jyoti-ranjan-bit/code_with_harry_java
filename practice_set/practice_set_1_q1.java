64a9c88a4f9e755c76ab17004ed2b5062ced8308
codex_jrp <118820439+jyoti-ranjan-bit@users.noreply.github.com>
2023-04-16 00:18:46 +0530

    Create practice_set_1_q1.java

---
+++ practice_set/practice_set_1_q1.java
@@ -0,0 +1,10 @@
+public class practice_set_1_q1 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+        int c=10;
+       int sum=a+b+c;
+        System.out.println(sum);
+
+    }
+}
